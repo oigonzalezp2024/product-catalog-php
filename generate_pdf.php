debfec253bd35676da225c43d36ea7e1c7680888
Oscar Gonzalez <oigonzalezp2024@gmail.com>
2025-08-04 22:32:44 -0500

    fix(generate_pdf.php): Ajustes de usabilidad

--- generate_pdf.php
+++ generate_pdf.php
@@ -2,14 +2,11 @@
 
 require __DIR__ . '/vendor/autoload.php';
 require('./src/Infrastructure/Lib/Fpdf/fpdf.php');
-
 require('./data.php');
-
 require('./src/Infrastructure/AbstractPDFGenerator.php');
 require('./src/Application/OrderPDFGenerator.php');
-
-use App\Domain\OrderPDFGeneratorInterface;
 
 // Creamos una nueva instancia del generador de PDF y la ejecutamos
 $pdf = new OrderPDFGenerator($page_info, $order_details);
 $pdf->generate();
+$pdf->Output('F','./pedidos/filename.pdf'); 
--- src/Application/OrderPDFGenerator.php
+++ src/Application/OrderPDFGenerator.php
@@ -1,13 +1,12 @@
 <?php
-require('./src/Infrastructure/AbstractPDFGenerator.php');
 
-use App\Domain\OrderPDFGeneratorInterface;
+use App\Domain\Interfaces\Entities\OrderPDFGeneratorInterface;
 
 /**
  * Clase concreta para la generación de PDFs de pedidos.
  * Hereda de AbstractPDFGenerator e implementa la interfaz de generación.
  */
-class OrderPDFGenerator extends AbstractPDFGenerator
+class OrderPDFGenerator extends AbstractPDFGenerator implements OrderPDFGeneratorInterface
 {
     /**
      * Método principal para generar y enviar el PDF completo.
@@ -23,6 +22,5 @@ class OrderPDFGenerator extends AbstractPDFGenerator
         $this->addPaymentTotal();
         $this->addPaymentMethods();
         $this->addFooter();
-        $this->Output();
     }
 }
